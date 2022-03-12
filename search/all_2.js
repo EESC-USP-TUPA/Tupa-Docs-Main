69e8a6af617f30855de4b5f020abad346de6f146
AndreyCortez <70648908+AndreyCortez@users.noreply.github.com>
2022-03-12 20:46:14 +0000

    Deploying to gh-pages from  @ d1cc04ffede72ff86494998e2495cca8c3d5f830 🚀

---
+++ search/all_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['tal_2',['TAL',['../md__docs_custom-pages__pagina_inicial.html',1,'']]],
+  ['tupa_20docs_20template_3',['Tupa Docs Template',['../md__r_e_a_d_m_e.html',1,'']]]
+];
---
+++ search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['paginainicial_2emd_4',['PaginaInicial.md',['../_pagina_inicial_8md.html',1,'']]]
+];
